1fa06dc0cd5c089295f925deb40baeeb32842e4d
RaphyStoll <raphael.ferreira@ik.me>
2024-07-26 17:22:32 +0200

    dsa

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: raphael <raphael@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 15:41:37 by raphael           #+#    #+#             */
-/*   Updated: 2024/07/26 16:16:42 by raphael          ###   ########.fr       */
+/*   Updated: 2024/07/26 17:21:45 by raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,3 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		}
 	}
 }
-sk-proj-4j7dZV004HgH1u403Y5aT3BlbkFJC8LoB2zgyl6ynkzFn4MU
